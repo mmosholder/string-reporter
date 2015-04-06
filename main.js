3c77aa61b0548f18f8adbf1618eed2e716dc1d18
Malinda Mosholder <m.m.mosholder@gmail.com>
2015-04-06 12:32:01 -0600

    added alerts

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 var word = prompt("give me a word!");
 if (word != null) {
 	alert("Your word is " + word + ". There are " + word.length + " characters in your word!")
+	alert("The third letter in your word is " + word.charAt(2))
+	alert("Here's how your word looks in all lowercase: " + word.toLowerCase())
+	alert("Here's how your word looks in all uppercase; " + word.toUpperCase())
+	alert("Example: " + word + ". " + "What a great word.")
+	alert("Subword: " + word.substring(1, 4))
 }
